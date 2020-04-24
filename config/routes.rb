bed36e3a92a7fc0c59af634dca2cfb888ab1ea53
Sam Naish <sam.naish.92@gmail.com>
2020-04-24 12:33:51 +0100

    add some styles, backup commit before installing devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
+
+
   get 'welcome/index'
   root 'welcome#index'
   get 'about', to: 'welcome#about', as: 'about'
-  get 'signup', to: 'users#new', as: 'signup'
-  get 'login', to: 'sessions#new', as: 'login'
+
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
