94864488aaa7d031a81ee9d7c6920633c778e7eb
Sam Naish <sam.naish.92@gmail.com>
2020-04-27 15:10:23 +0100

    feat: change/configured devise gem to allow user to login with username or email, added it to schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
+  attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def self.find_for_database_authentication wardern_condition
+    conditions = wardern_condition.dup
+    login = conditions.delete(:login)
+    where(conditions).where(
+      ["lower(username) = :value OR Lower(email) = value",
+    { value: login.strip.downcase}]).first
+  end
 end
