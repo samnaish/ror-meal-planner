375f8b1aa6ca731d0d579ca83169e30d7d696b25
Sam Naish <sam.naish.92@gmail.com>
2020-05-18 16:01:24 +0100

    working on search page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,6 +6,17 @@ class WelcomeController < ApplicationController
   end
 
   def search
+    if search_params[:search].present?
+      @recipe = Recipe.search(params[:search])
+    else
+      @recipe = Recipe.all
+    end
+  end
+
+  private
+
+  def search_params
+    params.permit(:recipe).permit(:name, :id, :search)
   end
 
 end
